2abeaf22a0a21e9f9a2c519612353afc6c2a1a35
sandtr0ut <crosbyverse@gmail.com>
2019-08-26 14:25:51 -0700

    heroku deploy

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -3,6 +3,10 @@ var path = require('path');
 // Routing
 
 module.exports = function(app) {
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
   app.get('/survey', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/survey.html'));
   });
